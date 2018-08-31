7e0cc719b7249facddea7fd2cf07e3d08e5fe034
Wu Lei <znss1989@gmail.com>
2018-08-31 17:55:50 +0800

    cast votes

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,17 +1,14 @@
 pragma solidity ^0.4.24;
 
 contract Election {
-    // Model a Candidate
+    // model a Candidate
     struct Candidate {
         uint id;
         string name;
         uint voteCount;
     }
-
-    // Read/write Candidates
-    mapping(uint => Candidate) public candidates;
-
-    // Store Candidates Count
+    mapping(uint => Candidate) public candidates; // read/write candidates
+    mapping(address => bool) public voters; // store accounts that have voted
     uint public candidatesCount;
 
     constructor() public {
@@ -22,5 +19,13 @@ contract Election {
     function addCandidate (string _name) private {
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId)  public {
+        require(!voters[msg.sender]); // require that they haven't voted before
+        require(_candidateId > 0 && _candidateId <= candidatesCount); // require a valid candiate
+
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
     }
 }
